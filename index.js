df75e6eb0e16cfc5e072339148838965f69332cd
Claire List <claire.list2501@gmail.com>
2021-09-15 14:26:35 -0600

    hacking out task 1

--- index.js
+++ index.js
@@ -39,10 +39,15 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name,age) {
+  this.name= name,
+  this.age= age,
+  stomach=[]
 }
 
+Person.prototype.eat = function(someFood){
+stomach.push(someFood);
+};
 
 
 
